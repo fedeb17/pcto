689bb42fd476c2875ee70059348a36722c9d7bca
fedeb17 <158043470+fedeb17@users.noreply.github.com>
2024-01-29 15:48:19 +0000

    Bonga è uno scroccone

---
+++ fileroma.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    char x = '&';
+    printf("kahoot %c \n" , x);
+    return(0);
+}
---
+++ lunedi17.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1 = 4
+    int n2 = 6
+    int risultato = n1 + n2;
+
+    printf("il tuo risultato è:%d", risultato);
+    return(0);
+}
